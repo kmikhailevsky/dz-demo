4b980266b5c82b54be2438001fe39d0228d5dfc6
kmikhailevsky <kmikhailevsky@yandex.ru>
2026-03-15 15:11:25 +1200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const currentTime = ref(Date.now());
+</script>
+
+<template>
+  <div class="box">
+    <h1>15.03.2026</h1>
+  </div>
+</template> 
+
+<style scoped>
+  .box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 4rem;
+  }
+
+</style>
